dc555685b332d7aee58095729e6eeb37c51d8df7
OZCAN AKYUZ <119737546+ozcanakyuz@users.noreply.github.com>
2025-01-25 10:42:29 +0300

    ChangePasswordViewModel

--- UserApp/ViewModels/ChangePasswordViewModel.cs
+++ UserApp/ViewModels/ChangePasswordViewModel.cs
@@ -2,11 +2,8 @@
 
 namespace UserApp.ViewModels
 {
-    public class RegisterViewModel
+    public class ChangePasswordViewModel
     {
-        [Required(ErrorMessage = "Name is required.")]
-        public string Name { get; set; }
-
         [Required(ErrorMessage = "Email is required.")]
         [EmailAddress]
         public string Email { get; set; }
@@ -14,12 +11,13 @@ namespace UserApp.ViewModels
         [Required(ErrorMessage = "Password is required.")]
         [StringLength(40, MinimumLength = 8, ErrorMessage = "The {0} must be at {2} and at max {1} characters long.")]
         [DataType(DataType.Password)]
-        [Compare("ConfirmPassword", ErrorMessage = "Password does not match.")]
-        public string Password { get; set; }
+        [Display(Name = "New Password")]
+        [Compare("ConfirmNewPassword", ErrorMessage = "Password does not match.")]
+        public string NewPassword { get; set; }
 
         [Required(ErrorMessage = "Confirm Password is required.")]
         [DataType(DataType.Password)]
-        [Display(Name = "Confirm Password")]
-        public string ConfirmPassword { get; set; }
+        [Display(Name = "Confirm New Password")]
+        public string ConfirmNewPassword { get; set; }
     }
 }
