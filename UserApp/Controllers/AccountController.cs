be527f19cbc272b101ff600a6faecdcce7e08d43
OZCAN AKYUZ <119737546+ozcanakyuz@users.noreply.github.com>
2025-01-25 11:48:41 +0300

    Register AccountController Ok.

--- UserApp/Controllers/AccountController.cs
+++ UserApp/Controllers/AccountController.cs
@@ -1,9 +1,21 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using UserApp.Models;
+using UserApp.ViewModels;
 
 namespace UserApp.Controllers
 {
     public class AccountController : Controller
     {
+        private readonly SignInManager<Users> signInManager;
+        private readonly UserManager<Users> userManager;
+
+        public AccountController(SignInManager<Users> signInManager, UserManager<Users> userManager)
+        {
+            this.signInManager = signInManager;
+            this.userManager = userManager;
+        }
+
         public IActionResult Login()
         {
             return View();
@@ -13,6 +25,38 @@ namespace UserApp.Controllers
         {
             return View();
         }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                Users users = new Users
+                {
+                    FullName = model.Name,
+                    Email = model.Email,
+                    UserName = model.Email,
+                };
+
+                var result = await userManager.CreateAsync(users, model.Password);
+
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Login", "Account");
+                }
+                else
+                {
+                    foreach (var error in result.Errors)
+                    {
+                        ModelState.AddModelError("", error.Description);
+                    } 
+
+                    return View(model);
+                }
+            }
+                return View(model);
+        }
+
         public IActionResult VerifyEmail()
         {
             return View();
