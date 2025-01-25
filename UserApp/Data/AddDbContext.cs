999bc248bf7113758e95d3c34a9ec07fe883618b
OZCAN AKYUZ <119737546+ozcanakyuz@users.noreply.github.com>
2025-01-25 09:56:12 +0300

    Data Folder>AddDbContext.cs

---
+++ UserApp/Data/AddDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using UserApp.Models;
+
+namespace UserApp.Data
+{
+    public class AddDbContext : IdentityDbContext<Users>
+    {
+        public AddDbContext(DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
